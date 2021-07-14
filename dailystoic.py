0d99b323f8c4928cb3248919b3dac3a7b09bda11
edg <glutenfreepy@gmail.com>
2021-07-14 08:53:39 -0700

    sample dict for testing api

---
+++ dailystoic.py
@@ -0,0 +1,11 @@
+messages = {
+    "july11": "test11",
+    "july12": "test12",
+    "july13": "test13",
+    "july14": "test14",
+    "july15": "test15",
+    "july16": "test16",
+    "july17": "test1j",
+    "july18": "test18",
+    "july19": "test19",
+}
