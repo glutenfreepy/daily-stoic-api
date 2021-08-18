d7ecffe8283db084a5a56e721715f69fe36d3e8a
edg <glutenfreepy@gmail.com>
2021-08-17 20:55:49 -0700

    check for status 200 before loading data

--- get_json_remote.py
+++ get_json_remote.py
@@ -1,11 +1,14 @@
 import requests
 URL = "https://raw.githubusercontent.com/IshanMi/Stoicbot/master/pdf_parsing/Stoic_log.json"
 
-# TODO: check for valid response and only proceed if valid
-data = requests.get(URL).json()
+response = requests.get(URL)
 
-# TODO: load data into Deta Base (key, stoic_title)
-for key in data:
-    stoic_title = data.get(f"{key}", {}).get("title")
-    print(key, stoic_title)
-    # db.put({"title": f'"{stoic_title}"'}, {"key": f'"{key}"'})
+if response.status_code == 200:
+    data = response.json()
+    # TODO: load data into Deta Base (key, stoic_title)
+    for key in data:
+        stoic_title = data.get(f"{key}", {}).get("title")
+        print(key, stoic_title)
+        # db.put({"title": f'"{stoic_title}"'}, {"key": f'"{key}"'})
+else:
+    break
