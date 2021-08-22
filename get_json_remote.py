ae67d2ae1563592966eab7246b06584cebe1e2ec
edg <glutenfreepy@gmail.com>
2021-08-22 12:55:16 -0700

    add if/else to account for missing keys in the downloaded json file

--- get_json_remote.py
+++ get_json_remote.py
@@ -1,23 +1,28 @@
-import re
 import requests
 from decouple import config
 from deta import Deta
 
 DETA_PROJECT_KEY = config('DETA_PROJECT_KEY')
 DB_NAME = config('DETA_DB_NAME')
+URL = "https://raw.githubusercontent.com/IshanMi/Stoicbot/master/pdf_parsing/Stoic_log.json"
 
 deta = Deta(DETA_PROJECT_KEY)
 db = deta.Base(DB_NAME)
 
-URL = "https://raw.githubusercontent.com/IshanMi/Stoicbot/master/pdf_parsing/Stoic_log.json"
 response = requests.get(URL)
 
 if response.status_code == 200:
     data = response.json()
     for key, val in data.items():
-        stoic_title = data.get(f"{key}", {}).get("title")
-        key = key.replace(" ", "_")
-        # print(f"{key}\n{stoic_title}\n\b")
-        db.put({"title": stoic_title, "key": key})
+        if key:
+            stoic_title = data.get(f"{key}", {}).get("title")
+            key = key.replace(" ", "_")
+            db.put({"title": stoic_title, "key": key})
+            # print(f"{key}\n{stoic_title}\n\b")
+        else:
+            key = None
+            db.put({"title": stoic_title, "key": key})
+            # print(f"{key}\n{stoic_title}\n\b")
+
 else:
     print(f'Bad Status Code: {response.status_code}')
