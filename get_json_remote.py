990cffe27fc8dabc4fea57403212993c7de6a97c
edg <glutenfreepy@gmail.com>
2021-08-22 18:12:16 -0700

    remove print lines

--- get_json_remote.py
+++ get_json_remote.py
@@ -16,12 +16,9 @@ if response.status_code == 200:
     for key, val in data.items():
         if key == "":
             db.put({"title": stoic_title})
-            print(f"{stoic_title}\n\b")
         else:
             stoic_title = data.get(f"{key}", {}).get("title")
-            print(type(key))
             key = key.replace(" ", "_")
             db.put({"title": stoic_title, "key": key})
-            print(f"{key}\n{stoic_title}\n\b")
 else:
     print(f'Bad Status Code: {response.status_code}')
